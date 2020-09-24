1f383a1b5b366d830b590bcc08b4fa44d623fe62
Code-Sploit <64798467+Code-Sploit@users.noreply.github.com>
2020-09-24 19:03:37 +0200

    Update fallbacks.h

--- include/fallbacks.h
+++ include/fallbacks.h
@@ -3,10 +3,17 @@
 using namespace std;
 
 // DEFINE CONSTANT FALLBACK VARIABLES:
-const float opacity = 0.0;
+
 const QString colorScheme = "Linux";
-const string editor = "/usr/bin/vim";
-const bool boldfont = false;
+
+const float opacity = 0.0;
+
 const int fontsize = 10;
+
+const bool boldfont = false;
+const bool italicfont = false;
+
 const string fonttype = "Monospace";
 const string defaultshell = "/bin/bash";
+const string editor = "/usr/bin/vim";
+const string term = "konsole-256color";
