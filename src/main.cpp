6fb232b67cae209cc203c35160a0b62a5a580a28
Code-Sploit <64798467+Code-Sploit@users.noreply.github.com>
2020-10-15 18:10:00 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 #include <QAction>
 #include <QDesktopServices>
 #include <QMenuBar>
+#include <QIcon>
 
 #include <iostream>
 #include <fstream>
@@ -16,92 +17,115 @@
 
 #include "qtermwidget.h"
 
-#include "../include/configP.h"
-#include "../include/menus.h"
+#include "configP.h"
+#include "menus.h"
+
+#include "../include/logger.h"
 
 using namespace std;
+using namespace menus;
 
 void setShell(string shellname) {
-	const char *converted_shellname = shellname.c_str();
-	setenv("SHELL", converted_shellname, 2);
+        const char *converted_shellname = shellname.c_str();
+        setenv("SHELL", converted_shellname, 2);
 }
 
 void setTerm(string termvalue) {
-	const char *converted_termvalue = termvalue.c_str();
-	setenv("TERM", converted_termvalue, 2);
+        const char *converted_termvalue = termvalue.c_str();
+        setenv("TERM", converted_termvalue, 2);
 }
 
 void setEditor(string editorvalue) {
-	const char *converted_editorvalue = editorvalue.c_str();
-	setenv("EDITOR", converted_editorvalue, 3);
+        const char *converted_editorvalue = editorvalue.c_str();
+        setenv("EDITOR", converted_editorvalue, 3);
 }
 
 main(int argc, char *argv[]) {
-	// Define configfile path
-	string configfile = getenv("HOME");
-	configfile.append("/.config/voidterm/voidterm.conf");
 
-	// Define mainWindows
-	QApplication app(argc, argv);
-	QMainWindow *mainWindow = new QMainWindow();
- 	
-	// Define some extra strings
-	const QString fonttype = QString::fromStdString(getFontTypeValue(configfile));
-	
-	// Set ENV variables
-	setTerm(getTermValue(configfile));
-	setShell(getDefaultShellValue(configfile));
-	setEditor(getDefaultEditorValue(configfile));
-	
-	// Define console
-	QTermWidget *console = new QTermWidget();
-	
-	// Set font
-	QFont font = QApplication::font();
-	
-	font.setFamily(fonttype);
-	font.setPointSize(getFontSizeValue(configfile));
-	font.setBold(getBoldFontValue(configfile));
-	font.setItalic(getItalicFontValue(configfile));
-	
-	// Set Configured settings
-	console->setColorScheme(getColorSchemeValue(configfile));
-	console->setTerminalFont(font);
-	console->setScrollBarPosition(QTermWidget::ScrollBarRight);
-	console->setTerminalOpacity(getOpacityValue(configfile));
-	
-	// Menu bar
-	QMenuBar *menuBar = new QMenuBar(mainWindow);
-	QMenu *actionsMenu = new QMenu("Actions", menuBar);
-	
-	if (checkForActionMenu(configfile) == 1) {
-		menuBar->addMenu(actionsMenu);
-		actionsMenu->addAction("Find..", console, SLOT(toggleShowSearchBar()), QKeySequence(Qt::CTRL, Qt::SHIFT, Qt::Key_F));
-		mainWindow->setMenuBar(menuBar);
-	}
-	else
-	{
-		// Do something in the else statement
-		string randomStringValue = "test";
-	}
-	
-	
-	// Launch the terminal && define keybindings
-	QObject::connect(console, &QTermWidget::termKeyPressed, mainWindow,
-			[=](const QKeyEvent *key) -> void {
-			if (key->matches(QKeySequence::Copy)) {
-				console->copyClipboard();
-			}
-			else if (key->matches(QKeySequence::Paste)) {
-				console->pasteClipboard();
-			}
-			else if (key->matches(QKeySequence::Quit)) {
-				exit(0);
-			}});
-	QObject::connect(console, SIGNAL(finished()), mainWindow, SLOT(close()));
+        // Create objects to Classes
+        Logger log;
 
-	mainWindow->setCentralWidget(console);
+        // Define configfile path
+        string configfile = getenv("HOME");
+        configfile.append("/.config/voidterm/voidterm.conf");
 
-	mainWindow->show();
-	return app.exec();
+        // Define mainWindow
+        QApplication app(argc, argv);
+        QMainWindow *mainWindow = new QMainWindow();
+
+        // Define some extra strings
+        const QString fonttype = QString::fromStdString(getFontTypeValue(configfile));
+
+        // Set ENV variables
+        setTerm(getTermValue(configfile));
+        setShell(getDefaultShellValue(configfile));
+        setEditor(getDefaultEditorValue(configfile));
+
+        log.log("Setted ENV variables.", 0);
+
+        // Define console
+        QTermWidget *console = new QTermWidget();
+
+        // Set font
+        QFont font = QApplication::font();
+
+        font.setFamily(fonttype);
+        font.setPointSize(getFontSizeValue(configfile));
+        font.setBold(getBoldFontValue(configfile));
+        font.setItalic(getItalicFontValue(configfile));
+
+        log.log("Setted font.", 0);
+
+        // Set Configured settings
+        console->setColorScheme(getColorSchemeValue(configfile));
+        console->setTerminalFont(font);
+        console->setScrollBarPosition(QTermWidget::ScrollBarRight);
+        console->setTerminalOpacity(getOpacityValue(configfile));
+
+        log.log("Setted console settings", 0);
+
+
+        // Resize to resonable window size
+        mainWindow->resize(1000, 1000);
+
+        log.log("Resized terminal to: 1000, 1000", 0);
+
+        // Menu bar
+        QMenuBar *menuBar = new QMenuBar(mainWindow);
+        QMenu *actionsMenu = new QMenu("Actions", menuBar);
+
+        if (checkForActionMenu(configfile) == 1) {
+                menuBar->addMenu(actionsMenu);
+                actionsMenu->addAction("Find..", console, SLOT(toggleShowSearchBar()), QKeySequence(Qt::CTRL, Qt::SHIFT, Qt::Key_F));
+                mainWindow->setMenuBar(menuBar);
+
+                log.log("Added action menu", 0);
+        }
+        else
+        {
+                // Do something in the else statement
+                string randomStringValue = "value";
+        }
+
+        // Launch the terminal && define keybindings
+        QObject::connect(console, &QTermWidget::termKeyPressed, mainWindow,
+                        [=](const QKeyEvent *key) -> void {
+                        if (key->matches(QKeySequence::Copy)) {
+                                console->copyClipboard();
+                                log.log("User used copy shortcut (Ctrl-C).", 0);
+                        }
+                        else if (key->matches(QKeySequence::Paste)) {
+                                console->pasteClipboard();
+                                log.log("User used paste shortcut (Ctrl-V).", 0);
+                        }
+                        else if (key->matches(QKeySequence::Quit)) {
+                                log.log("Voidterm exited.", 0);
+                                exit(0);
+                        }});
+        QObject::connect(console, SIGNAL(finished()), mainWindow, SLOT(close()));
+
+        mainWindow->setCentralWidget(console);
+
+        mainWindow->show();
+        return app.exec();
 }
